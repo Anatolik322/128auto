f321c40200d0506932fc2a967434b01d4fd78850
Anatolik322 <anatoliysynecun2016@gmail.com>
2024-09-03 11:52:20 +0300

    chore: add variables

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,6 +2,7 @@ import './Landing.css'
 import land from '../../asset/brand/men2.png'
 import { Link } from "react-router-dom"
 import { Button } from "@mui/material";
+import vacum from "../../asset/abc.png"
 
 const Landing = () => {
     return (
@@ -9,14 +10,14 @@ const Landing = () => {
             <div className="landing__header__container">
                 <div className="landing__header">
                     <h3 className="landing__header__discount">UP TO 15% DISCOUNT</h3>
-                    <h1 className="landing__header__main">Checkout The Best Fashion Style</h1>
+                    <h1 className="landing__header__main">Unleash the Power of Clean with Our Car Vacuum Cleaner!</h1>
                     <Link to="/shop">
                         <Button variant='outlined' sx={[{ width: '190px', marginTop: '10px', height: '50px', borderRadius: '20px', fontWeight: '700', backgroundColor: 'none', borderColor: '#fff', color: '#fff' }, { '&:hover': { backgroundColor: "white", color: "#F28A0A", borderColor: '#fff' } }]}>SHOP NOW</Button>
                     </Link>
                 </div>
             </div>
             <div className="landing__image__container">
-                <img className="landing__image !w-[100px] !h-[100px]" src='/asset/abc.png' alt="" />
+                <img className="landing__image " src={vacum} alt="car vacuum" />
             </div>
         </div>
     );
--- src/hooks/AxiosInstans.js
+++ src/hooks/AxiosInstans.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.MONGO_URI,
 });
 
 export default api;
