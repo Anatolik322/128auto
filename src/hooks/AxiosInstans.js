455ff87d385c55d65902b574b1d27c25bc83ba1e
Anatolik322 <anatoliysynecun2016@gmail.com>
2024-09-03 12:13:14 +0300

    fix: fixed url

--- src/hooks/AxiosInstans.js
+++ src/hooks/AxiosInstans.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+console.log(process.env.REACT_APP_API_URL);
 
 const api = axios.create({
-    baseURL: process.env.MONGO_URI,
+    baseURL: 'https://128autoapi.vercel.app',
 });
 
 export default api;
